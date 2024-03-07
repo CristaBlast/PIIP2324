a80773742f2a1d0a538d1901c59642e5cc725197
Cris <cristianoben10@gmail.com>
2024-03-07 17:24:59 +0000

    07032024.1.2

--- TrabalhoLaboratorialProjeto/src/main.java
+++ TrabalhoLaboratorialProjeto/src/main.java
@@ -4,5 +4,7 @@ public class main {
         Professor professor2=new Professor("Jão",2);
         Aula aula1=new Aula("Programação II",69);
         Aula aula2=new Aula("Programação Abançada",6);
+        Aluno aluno1=new Aluno("Gerbásio",46);
+        Aluno aluno2=new Aluno("Anabásio",663);
     }
 }
